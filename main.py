ead7236292fa2e950ce5fdc46932a12602b7faa5
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2026-07-06 06:14:09 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import pygame as pg
+import json
+from random import random
+
+
+TIMEOUT = 3
+SIZE = WIDTH, HEIGHT = 800, 600
+
+# Colors
+BACKGROUND = "#2222aa"
+CARD = "#ffffff"
+TEXT = "#000000"
+
+pg.init()
+calibri = pg.font.SysFont("Calibri", 24)
+pg.display.set_caption("Flashcards")
+screen = pg.display.set_mode(SIZE)
+clock = pg.time.Clock()
+
+surf_timer = pg.Surface((WIDTH*0.1, HEIGHT*0.1))
+surf_timer.fill(CARD)
+
+surf_card = pg.Surface((WIDTH*0.666, HEIGHT*0.666))
+surf_card.fill(CARD)
+
+surf_image = pg.Surface((WIDTH*0.5, HEIGHT*0.5))
+surf_image.fill(CARD)
+
+PATH_DECK = "deck.json"
+with open(PATH_DECK, "r") as file:
+    deck = json.load(file)
+# backup file
+with open("." + PATH_DECK, "w") as file:
+    json.dump(deck, file, indent=2)
+
+timer = 0
+
+question = True
+choice = round(random())
+image = pg.image.load(deck[choice]["question" if question else "answer"]).convert()
+surf_image.blit(pg.transform.smoothscale(image, (WIDTH*0.5, HEIGHT*0.5)), (0, 0))
+
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                with open(PATH_DECK, "w") as file:
+                    json.dump(deck, file, indent=2)
+                pg.quit()
+                exit()
+            elif question and event.key == pg.K_SPACE:
+                question = not question
+                choice = round(random())
+                image = pg.image.load(deck[choice]["question" if question else "answer"]).convert()
+                surf_image.blit(pg.transform.smoothscale(image, (WIDTH*0.5, HEIGHT*0.5)), (0, 0))
+            elif not question and event.key == pg.K_t:
+                question = not question
+                choice = round(random())
+                image = pg.image.load(deck[choice]["question" if question else "answer"]).convert()
+                surf_image.blit(pg.transform.smoothscale(image, (WIDTH*0.5, HEIGHT*0.5)), (0, 0))
+                deck[choice]["true"] += 1
+                timer = 0
+            elif not question and event.key == pg.K_f:
+                question = not question
+                choice = round(random())
+                image = pg.image.load(deck[choice]["question" if question else "answer"]).convert()
+                surf_image.blit(pg.transform.smoothscale(image, (WIDTH*0.5, HEIGHT*0.5)), (0, 0))
+                deck[choice]["false"] += 1
+                timer = 0
+
+    # timer-limit
+    if timer > TIMEOUT:
+        timer = 0
+        choice = round(random())
+        image = pg.image.load(deck[choice]["question" if question else "answer"]).convert()
+        surf_image.blit(pg.transform.smoothscale(image, (WIDTH*0.5, HEIGHT*0.5)), (0, 0))
+        deck[choice]["false"] += 1
+
+    screen.fill(BACKGROUND)
+
+    # Needs centering
+    screen.blit(surf_card, (120, 100))
+
+    # Needs centering
+    screen.blit(surf_image, (180, 140))
+
+    surf_timer.fill(CARD)
+    surf_timer.blit(calibri.render(f"{int(timer)}", antialias=True, color=TEXT), (0, 0))
+    screen.blit(surf_timer, (120, 100))
+
+    pg.display.update()
+    clock.tick(30)
+    if question:
+        timer += 1/30
